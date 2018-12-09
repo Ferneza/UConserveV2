4392a88b823bede06971578253501772183b91ac
smccullough <sgmccullough@umass.edu>
2018-12-09 16:33:42 -0500

    p

--- app/src/main/java/com/example/nfern/uconserve/GoogleMaps.java
+++ app/src/main/java/com/example/nfern/uconserve/GoogleMaps.java
@@ -8,9 +8,67 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowCloseListener;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowLongClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.annotation.ColorInt;
+import android.support.annotation.DrawableRes;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.Interpolator;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class GoogleMaps extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final LatLng CURRLOCATION = new LatLng(42.393132, -72.527962);
+    private static final LatLng BLUEWALL = new LatLng(42.391942, -72.526715);
+    private static final LatLng LIBRARY = new LatLng(42.389985, -72.527820);
+    private static final LatLng MORILL = new LatLng(42.389763, -72.524462);
+    private static final LatLng GODELL = new LatLng(42.388765, -72.529000);
+
+    private Marker mCurrlocation;
+    private Marker mBluewall;
+    private Marker mLibrary;
+    private Marker mMorill;
+    private Marker mGodell;
+
 
     private GoogleMap mMap;
 
@@ -37,10 +95,41 @@ public class GoogleMaps extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        LatLng UMass = new LatLng(42.391157, -72.526921);
-        mMap.addMarker(new MarkerOptions().position(UMass).title("UMass Amherst"));
+        addMarkersToMap();
         float zoomLevel = 15.0f;
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UMass, zoomLevel));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CURRLOCATION, zoomLevel));
+    }
+
+    private void addMarkersToMap() {
+        mCurrlocation = mMap.addMarker(new MarkerOptions()
+                .position(CURRLOCATION)
+                .title("Your Location"));
+
+        mBluewall = mMap.addMarker(new MarkerOptions()
+                .position(BLUEWALL)
+                .title("Blue Wall")
+                .snippet("Type: Trash, Recyling, Compost | Pinned By: Nick | Date: 12/10/18"));
+
+        mLibrary = mMap.addMarker(new MarkerOptions()
+                .position(LIBRARY)
+                .title("Library")
+                .snippet("Type: Trash, Recyling \n Pinned By: Joe \n Date: 12/2/18"));
+
+        mMorill = mMap.addMarker(new MarkerOptions()
+                .position(MORILL)
+                .title("Morill")
+                .snippet("Type: Trash \n Pinned By: John \n Date: 12/8/18"));
+
+        mGodell = mMap.addMarker(new MarkerOptions()
+                .position(GODELL)
+                .title("Godell")
+                .snippet("Type: Recyling \n Pinned By: Bill \n Date: 12/10/18"));
+
+        mCurrlocation.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+        mBluewall.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        mLibrary.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        mMorill.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        mGodell.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+
     }
 }
